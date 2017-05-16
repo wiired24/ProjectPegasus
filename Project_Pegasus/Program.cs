f93856cfef564d89addf2b93d9d79894e949f78e
wiired24 <vanitas131@gmail.com>
2017-05-16 13:30:17 -0500

    Update Program.cs
    Implemented a while loop to allow for multiple downloads

--- Project_Pegasus/Program.cs
+++ Project_Pegasus/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,10 @@ namespace Project_Pegasus
     {
         static void Main(string[] args)
         {
+            // Setting up loop to allow repeated multiple downloads
+            while (1 == 1)
+            {
+
             // Setting up the design of the Console Interface
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("________________________________________________                          ");
@@ -20,7 +24,7 @@ namespace Project_Pegasus
             Console.WriteLine("                                                                          ");
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(" # Note: If running on Windows make sure to create dir C:Users//Public//homebrew");
+            Console.WriteLine(" # Note: Make sure to create dir C:Users//Public//homebrew");
             Console.WriteLine(" If you have not already. Project Pegasus needs this directory to download");
             Console.WriteLine(" your files. More information can be located in the README file.");
             Console.ResetColor();
@@ -42,101 +46,102 @@ namespace Project_Pegasus
             Console.WriteLine("                                       |");
             Console.WriteLine("---------------------------------------");
 
-            
-
-
-            // Converting user selection from string to int for the switch block
-            string menuChoice_string = Console.ReadLine();
-            int menuChoice = Convert.ToInt32(menuChoice_string);
 
 
 
-           
-            // Setting uo switch block to handle user selections
-            switch (menuChoice)
-            {
-
-
-                 // Using the WebClient Class to connect to the server and download the file to the specified path
-                case 1:                    
-                    Console.WriteLine("Downloading Chip8 Please Wait....");
-                    WebClient Client = new WebClient();
-                    Client.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBbWpNQ19hUlc4RnM", @"C:\Users\Public\homebrew\CHIP8_XBOX_ONE_V3.rar");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-                    break;
-                    
-                   
-
-
-                case 2:
-                    Console.WriteLine("Downloading Nesbox Please Wait....");
-                    WebClient Client2 = new WebClient();
-                    Client2.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBcW9xMWw0Wlg2UjQ", @"C:\Users\Public\homebrew\NESBOX+RELEASE2+APPX+PKG.rar");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-
-                    break;
+                // Converting user selection from string to int for the switch block
+                string menuChoice_string = Console.ReadLine();
+                int menuChoice = Convert.ToInt32(menuChoice_string);
 
 
 
-                case 3:
-                    Console.WriteLine("Downloading PSX-BOX Please Wait....");
-                    WebClient Client3 = new WebClient();
-                    Client3.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBckdaXzJ0U0dSMVE", @"C:\Users\Public\homebrew\psx-box-master.zip");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
 
-                    break;
+                // Setting uo switch block to handle user selections
+                switch (menuChoice)
+                {
 
 
-                case 4:
-                    Console.WriteLine("Downloading PPPSSPP Please Wait....");
-                    WebClient Client4 = new WebClient();
-                    Client4.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBRlk3N1BHQkpRYmM", @"C:\Users\Public\homebrew\PPSSPP+Release+APPX+PKG.rar");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-
-                    break;
-
-
-                case 5:
-                    Console.WriteLine("Downloading VBA10 Please Wait....");
-                    WebClient Client5 = new WebClient();
-                    Client5.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBb25qVjJQRE1iUEE", @"C:\Users\Public\homebrew\VBA10_1.22.197.0_Test.rar");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-
-                    break;
+                    // Using the WebClient Class to connect to the server and download the file to the specified path
+                    case 1:
+                        Console.WriteLine("Downloading Chip8 Please Wait....");
+                        WebClient Client = new WebClient();
+                        Client.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBbWpNQ19hUlc4RnM", @"C:\Users\Public\homebrew\CHIP8_XBOX_ONE_V3.rar");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+                        break;
 
 
-                case 6:
-                    Console.WriteLine("Downloading Win64e10 Please Wait....");
-                    WebClient Client6 = new WebClient();
-                    Client6.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBTWJPeWhZU0hXRTQ", @"C:\Users\Public\homebrew\Win64e10+PKG.rar");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-
-                    break;
 
 
-                 case 7:
-                    Console.WriteLine("Downloading Dungeon Run Please Wait....");
-                    WebClient Client7 = new WebClient();
-                    Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBdmRJajZYazVFR0k", @"C:\Users\Public\homebrew\DungeonRUN1.rar");
-                    Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBeVo2Y29NMWNJT2c", @"C:\Users\Public\homebrew\DungeonRUN2.rar");
-                    Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBbGFUN1JvT0t1cWs", @"C:\Users\Public\homebrew\DungeonRUN3.rar");
-                    Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBMk1tbHJObUNXQnM", @"C:\Users\Public\homebrew\DungeonRun4.rar");
-                    Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBWjBtb1pOVVJId3c", @"C:\Users\Public\homebrew\DungeonRun_README");
-                    Console.WriteLine("Succesfully Downloaded...... :-)");
-                    break;
+                    case 2:
+                        Console.WriteLine("Downloading Nesbox Please Wait....");
+                        WebClient Client2 = new WebClient();
+                        Client2.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBcW9xMWw0Wlg2UjQ", @"C:\Users\Public\homebrew\NESBOX+RELEASE2+APPX+PKG.rar");
+                        Console.WriteLine("Succesfully Downloaded Press Enter to Download Again...... :-)");
+
+                        break;
+
+
+
+                    case 3:
+                        Console.WriteLine("Downloading PSX-BOX Please Wait....");
+                        WebClient Client3 = new WebClient();
+                        Client3.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBckdaXzJ0U0dSMVE", @"C:\Users\Public\homebrew\psx-box-master.zip");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+
+                        break;
+
+
+                    case 4:
+                        Console.WriteLine("Downloading PPPSSPP Please Wait....");
+                        WebClient Client4 = new WebClient();
+                        Client4.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBRlk3N1BHQkpRYmM", @"C:\Users\Public\homebrew\PPSSPP+Release+APPX+PKG.rar");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+
+                        break;
+
+
+                    case 5:
+                        Console.WriteLine("Downloading VBA10 Please Wait....");
+                        WebClient Client5 = new WebClient();
+                        Client5.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBb25qVjJQRE1iUEE", @"C:\Users\Public\homebrew\VBA10_1.22.197.0_Test.rar");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+
+                        break;
+
+
+                    case 6:
+                        Console.WriteLine("Downloading Win64e10 Please Wait....");
+                        WebClient Client6 = new WebClient();
+                        Client6.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBTWJPeWhZU0hXRTQ", @"C:\Users\Public\homebrew\Win64e10+PKG.rar");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+
+                        break;
+
+
+                    case 7:
+                        Console.WriteLine("Downloading Dungeon Run Please Wait....");
+                        WebClient Client7 = new WebClient();
+                        Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBdmRJajZYazVFR0k", @"C:\Users\Public\homebrew\DungeonRUN1.rar");
+                        Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBeVo2Y29NMWNJT2c", @"C:\Users\Public\homebrew\DungeonRUN2.rar");
+                        Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBbGFUN1JvT0t1cWs", @"C:\Users\Public\homebrew\DungeonRUN3.rar");
+                        Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBMk1tbHJObUNXQnM", @"C:\Users\Public\homebrew\DungeonRun4.rar");
+                        Client7.DownloadFile("https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBWjBtb1pOVVJId3c", @"C:\Users\Public\homebrew\DungeonRun_README");
+                        Console.WriteLine("Succesfully Downloaded..... Press Enter to Download Again :-)");
+                        break;
 
 
                     // Throws an Exception if the user pressed an invalid option
-                default:
-                    Console.WriteLine("Exception! Invalid Input Try Again");
-                    break;
+                    default:
+                        Console.WriteLine("Exception! Invalid Input Try Again");
+                        break;
 
 
 
 
+                }
+
+                Console.ReadLine();
             }
-
-            Console.ReadLine();
         }
     }
 }
