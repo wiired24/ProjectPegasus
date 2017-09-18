d82ec1f98ca1c12ccdec516e34a4dd9d5bba2e3c
wiired24 <vanitas131@gmail.com>
2017-09-17 18:42:53 -0700

    Update ProjectPegasus.sh

--- scripts/ProjectPegasus.sh
+++ scripts/ProjectPegasus.sh
@@ -14,8 +14,7 @@ c.Retrix    - (Libretro Port)           |
 d.PPSSPP    - (PSP Games)               |
 e.VBA10     - (GBA Games)               |
 f.Win64e10  - (N64 Games)               |
-                                        | 
-                                        | 
+                                        |  
     [Xbox One Homebrew Games]           |
 g.Dungeon Run - 2D Zelda Clone          |
 h.Hedgephysics - Fan made sonic game    |                                  |
@@ -24,33 +23,34 @@ i.Sonic Realms - New fan made sonic game|
 [Miscellaneous/Tools]                   |
 j.WinIRC - IRC Chat Client              |
 k.Boxify - Spotify Music Client         |
+l.UWPStreamer - Stream 3DS to XB1       |
 ----------------------------------------"
     
 
-while getopts ':abcdefghijk' option; do
+while getopts ':abcdefghijkl' option; do
   case "$option" in
     h) echo "$usage" >&2
        exit
        ;;
     a) echo "Downloading Chip8!" >&2
-       curl -L "https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBbWpNQ19hUlc4RnM" -o ~/Desktop/chip8.rar
+       curl -L "https://github.com/wiired24/ProjectPegasus/releases/download/appx_chip8/CHIP8.rar" -o ~/Desktop/chip8.rar
        exit
        ;;
     b) echo "Downloading Nesbox!" >&2
-       curl -L "https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBcW9xMWw0Wlg2UjQ" -o ~/Desktop/nesbox.rar
+       curl -L "https://github.com/wiired24/ProjectPegasus/releases/download/appx_nesbox/Nesbox.zip" -o ~/Desktop/nesbox.zip
        exit
        ;;
     c) echo "Downloading Retrix!" >&2
-       curl -L "https://github.com/Aftnet/LibretroRT/releases/download/v1.8/RetriX.UWP_1.8.14.0_x86_x64_arm.appxbundle" -o ~/Desktop/Retrix_v.1.8.zip
-       curl -L "https://github.com/Aftnet/LibretroRT/releases/download/v1.8/RetriX.UWP_1.8.14.0_x86_x64_arm.dependencies.zip" -o ~/Desktop/Retrix_v.1.8.dependencies.zip
+       curl -L "https://github.com/wiired24/ProjectPegasus/releases/download/appx_retrix/RetriX_v1.8.zip" -o ~/Desktop/Retrix.zip
+       
        exit
        ;;
     d) echo "Downloading PPSSPP!" >&2
-       curl -L  "https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBRlk3N1BHQkpRYmM" -o ~/Desktop/ppsspp.rar
+       curl -L  "https://github.com/wiired24/ProjectPegasus/releases/download/appx_ppsspp/PPSSPP.rar" -o ~/Desktop/PPSSPP.rar
        exit
        ;;
     e) echo "Downloading VBA10!" >&2
-       curl -L  "https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBb25qVjJQRE1iUEE" -o ~/Desktop/vba10.rar
+       curl -L  "https://github.com/wiired24/ProjectPegasus/releases/download/appx_vba10/VBA10.rar" -o ~/Desktop/VBA10.rar
        exit
        ;;
     f) echo "Downloading Win64e10!" >&2
@@ -90,6 +90,11 @@ while getopts ':abcdefghijk' option; do
        curl -L "https://drive.google.com/uc?export=download&id=0B6C6WFjsJozBWTYxYW0tNFFTRjQ" -o ~/Desktop/Boxify.zip
        exit
        
+       
+     l) echo "Downloading UWPStreamer!" >&2
+       curl -L "https://raw.githubusercontent.com/wiired24/ProjectPegasus/appx_releases/UWPstreamer.zip" -o ~/Desktop/UWPStreamer.zip
+       exit
+              
 	   ;;
 	   
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
